699c594ebf846f56cc4fe18b2d9108f7d5c61ac2
pawara-mmns <pawarasamarawickrama@gmail.com>
2025-11-16 10:41:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind is working 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
